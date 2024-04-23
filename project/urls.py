daf8e54efb6964b4bf9dc47ff16677f2edc62b42
ymk74 <swimmer.shark12@gmail.com>
2024-04-23 14:23:32 +0200

    create new super user

--- project/urls.py
+++ project/urls.py
@@ -5,11 +5,10 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('core.urls'))
 
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
