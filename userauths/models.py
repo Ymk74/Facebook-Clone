0e235395f97e9d3c0e1310f7479ea2dcbb0e5c6f
ymk74 <swimmer.shark12@gmail.com>
2024-04-23 15:33:23 +0200

    add slug in profile model

--- userauths/models.py
+++ userauths/models.py
@@ -93,3 +93,14 @@ class Profile(models.Model):
 
 
 # create user profile automatic
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile ,sender=User)
+post_save.connect(save_user_profile ,sender=User)
